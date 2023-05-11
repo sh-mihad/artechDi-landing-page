82ea7a64f3fc147aeac126f303e3ac01b21667cc
sh-mihad <sh.mihad.cse@gmail.com>
2023-05-11 20:05:05 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./utils/Navbar"
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/utils/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <h1 className="bigTitle text-[130px] font-BebasNeue"> hello font</h1>
+        </nav>
+    );
+};
+
+export default Navbar;
