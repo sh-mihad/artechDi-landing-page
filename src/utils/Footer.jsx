0ca1cdadecfd5afbe3ee698ec4fa8a367a39dc48
sh-mihad <sh.mihad.cse@gmail.com>
2023-05-14 02:09:52 +0600

    home page done

--- src/utils/Footer.jsx
+++ src/utils/Footer.jsx
@@ -1,11 +1,11 @@
-import { AiFillInstagram,AiOutlineTwitter } from "react-icons/ai";
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 import { FaDiscord, FaLinkedin, FaRegEnvelope } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <footer className="lg:w-9/12 mx-auto mt-32 ">
+        <footer className="lg:w-9/12 mx-auto mt-32 px-10 lg:px-0">
             {/* footer top part */}
-            <section className="flex items-center text-white  justify-between mb-10">
+            <section className=" flex flex-col lg:flex-row text-center lg:text-start items-center text-white  justify-between mb-10">
                 <div> <a className="text-2xl bigTitle font-bold">FANDORA</a></div>
                 <div className="block lg:flex items-center gap-6 font-semibold ">
                     <p><a href="">Home</a></p>
@@ -17,24 +17,25 @@ const Footer = () => {
             <hr />
             {/* footer bottom part */}
             <section className="mt-7">
-                <div  className="block lg:flex justify-between items-center gap-6 font-semibold ">
+                <div className="flex flex-col lg:flex-row justify-center lg:justify-between items-center gap-6 font-semibold ">
                     <div className="flex items-center gap-3 text-2xl text-white">
-                        <p><AiOutlineTwitter/></p>
-                        <p><FaLinkedin/></p>
-                        <p><AiFillInstagram/></p>
-                        <p><FaDiscord/></p>
-                        <p><FaRegEnvelope/></p>
-                        <p className="font-sans font-light text-base"><small>reachus@fandora.app</small></p>
+                        <p><AiOutlineTwitter /></p>
+                        <p><FaLinkedin /></p>
+                        <p><AiFillInstagram /></p>
+                        <p><FaDiscord /></p>
+                        <p><FaRegEnvelope /></p>
+                        <p className="font-sans font-light text-base lg:block hidden"><small>reachus@fandora.app</small></p>
                     </div>
-                    <div className="flex items-center gap-10 text-2xl text-white">
+                    <div className="flex flex-col lg:flex-row justify-center lg:justify-between  items-center gap-2 lg:gap-10 text-2xl text-white">
+                        <p className="font-sans font-light text-base lg:hidden block"><small>reachus@fandora.app</small></p>
                         <p className="font-sans font-light text-base"><small>Privacy Policy</small></p>
                         <p className="font-sans font-light text-base"><small>Terms of use</small></p>
                         <p className="font-sans font-light text-base"><small>© Fandora 2022 - All rights reserved </small></p>
                     </div>
                 </div>
                 <div className="flex justify-between text-white my-5 items-center">
-                <p className="font-sans font-light text-base"><small>Designed by @sabbir_hossen</small></p>
-                <p className="font-sans font-light text-base"><small>Fandora 1364019 B.C Ltd C/o Empowered Startups 744 W Hastings St #420, Vancouver, BC V6C 1A5, Canada</small></p>
+                    <p className="font-sans font-light text-base"><small>Designed by @sabbir_hossen</small></p>
+                    <p className="font-sans font-light text-base"><small>Fandora 1364019 B.C Ltd C/o Empowered Startups 744 W Hastings St #420, Vancouver, BC V6C 1A5, Canada</small></p>
                 </div>
             </section>
         </footer>
